8963b7ad3e007d4664dfee3c58af4de8cceb2a7b
Thomas Reppesgård <thr@digdir.no>
2023-05-15 10:02:12 +0200

    ID-2911: Forenkle API for applikasjoner som ikke har parsede X509-sertifikater (#57)
    * CRL updates
    
    * CRL updates
    
    * API-metoder som kan ta inn sertifikater som String

--- src/main/java/no/idporten/seid2/SEID2CertificateValidator.java
+++ src/main/java/no/idporten/seid2/SEID2CertificateValidator.java
@@ -1,5 +1,6 @@
 package no.idporten.seid2;
 
+import lombok.extern.slf4j.Slf4j;
 import no.digdir.certvalidator.Validator;
 import no.digdir.certvalidator.api.CertificateValidationException;
 
@@ -9,6 +10,7 @@ import java.util.Objects;
 /**
  * SEID2 certificate validator for X509 certificates.
  */
+@Slf4j
 public class SEID2CertificateValidator {
     private final Validator validator;
 
@@ -20,12 +22,25 @@ public class SEID2CertificateValidator {
         validator.validate(certificate);
     }
 
+    public void validate(String certificate) throws CertificateValidationException {
+        this.validate(X509CertificateUtils.readX509Certificate(certificate));
+    }
+
     public boolean isValid(X509Certificate certificate) {
         try {
             validator.validate(certificate);
             return true;
         } catch (CertificateValidationException e) {
-            // TODO hvordan kan lib logge?
+            log.error("Invalid certificate", e);
+            return false;
+        }
+    }
+
+    public boolean isValid(String certificate) {
+        try {
+            return this.isValid(X509CertificateUtils.readX509Certificate(certificate));
+        } catch (CertificateValidationException e) {
+            log.error("Invalid certificate", e);
             return false;
         }
     }
